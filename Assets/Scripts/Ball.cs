fae49e666a5729725e43a3b30c28e257ce8b5438
Brian Lai <brianlai30@outlook.com>
2018-02-24 17:27:17 -0800

    Uploading existing Unity project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    Camera cam;
+    public AudioClip bounce;
+    private Paddle paddle;
+    private Vector3 paddleToBallVector;
+    private bool hasStarted = false;
+
+    // Use this for initialization
+    void Start() {
+        this.cam = Camera.main;
+        paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+        print(paddleToBallVector.y);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (!hasStarted) {
+            // Lock the ball relative to the paddle.
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            // Wait for a mouse press to launch.
+            if (Input.GetMouseButtonDown(0)) {
+                Debug.Log("Mouse clicked, launch ball");
+                hasStarted = true;
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(3f, 10f);
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        Vector2 tweak = new Vector2(Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+        if (hasStarted) {
+            AudioSource.PlayClipAtPoint(bounce, this.cam.transform.position);
+            this.GetComponent<Rigidbody2D>().velocity += tweak;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public float minX, maxX;
+	public bool autoPlay = false;
+	private Ball ball;
+
+	    // Use this for initialization
+    void Start() {
+		ball = GameObject.FindObjectOfType<Ball>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (!autoPlay) {
+        	MoveWithMouse();
+		} else {
+			AutoPlay();
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			if (autoPlay) {
+				autoPlay = false;
+			} else if (!autoPlay) {
+				autoPlay = true;
+			}
+		}
+	}
+
+	void MoveWithMouse() {
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
+
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
+		this.transform.position = paddlePos;
+	}
+
+	void AutoPlay() {
+		Vector3 ballPos = ball.transform.position;
+        Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
+
+        paddlePos.x = Mathf.Clamp(ballPos.x, minX, maxX);
+		this.transform.position = paddlePos;
+	}
+
+}
